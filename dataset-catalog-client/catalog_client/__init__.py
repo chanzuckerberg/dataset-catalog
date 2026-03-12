a183544446e8548b4bf5370822db1b136c4a4552
mvenkatakrishnan <mvenkatakrishnan@gmail.com>
2026-03-11 21:08:37 -0700

    feat: add governance, quality, and metadata models

--- dataset-catalog-client/catalog_client/__init__.py
+++ dataset-catalog-client/catalog_client/__init__.py
@@ -1,87 +1,15 @@
+# catalog_client package
+# NOTE: This __init__.py is being rewritten. See Task 14 for the final version.
+# The old imports are removed to allow the new models package to be built
+# incrementally. The full public API will be re-exported after all tasks complete.
+
 from catalog_client._context import get_client
-from catalog_client.client import CatalogClient, CollectionClient, DatasetClient, LineageClient, TokenClient
 from catalog_client.exceptions import AuthenticationError, CatalogError, CatalogServerError, NotFoundError
-from catalog_client.models import (
-    APITokenCreatedResponse,
-    APITokenResponse,
-    AssetType,
-    CollectionCreate,
-    CollectionResponse,
-    CollectionUpdate,
-    DataAssetRequest,
-    DataAssetResponse,
-    DataSummaryMetadata,
-    DatasetCreate,
-    DatasetMetadata,
-    DatasetModality,
-    DatasetResponse,
-    DatasetType,
-    DatasetUpdate,
-    ExperimentMetadata,
-    GovernanceMetadata,
-    LineageEdgeCreate,
-    LineageEdgeResponse,
-    LineageType,
-    OntologyEntry,
-    PaginatedResponse,
-    SampleMetadata,
-    TissueEntry,
-    TokenExpiry,
-    TokenIssueRequest,
-    TokenUserResponse,
-    TokenValidateRequest,
-    TokenValidateResponse,
-)
 
 __all__ = [
-    # Clients
-    "CatalogClient",
-    "CollectionClient",
-    "DatasetClient",
-    "LineageClient",
-    "TokenClient",
-    # Exceptions
-    "CatalogError",
-    "AuthenticationError",
-    "NotFoundError",
-    "CatalogServerError",
-    # Enums
-    "AssetType",
-    "DatasetModality",
-    "DatasetType",
-    "LineageType",
-    "TokenExpiry",
-    # Asset models
-    "DataAssetRequest",
-    "DataAssetResponse",
-    # Dataset models
-    "DatasetCreate",
-    "DatasetUpdate",
-    "DatasetResponse",
-    # Metadata models
-    "DatasetMetadata",
-    "GovernanceMetadata",
-    "ExperimentMetadata",
-    "SampleMetadata",
-    "DataSummaryMetadata",
-    "OntologyEntry",
-    "TissueEntry",
-    # Collection models
-    "CollectionCreate",
-    "CollectionUpdate",
-    "CollectionResponse",
-    # Lineage models
-    "LineageEdgeCreate",
-    "LineageEdgeResponse",
-    # Pagination
-    "PaginatedResponse",
-    # Token models
-    "TokenIssueRequest",
-    "TokenValidateRequest",
-    "TokenValidateResponse",
-    "APITokenResponse",
-    "APITokenCreatedResponse",
-    "TokenUserResponse",
-    # Utilities
     "get_client",
+    "AuthenticationError",
+    "CatalogError",
+    "CatalogServerError",
+    "NotFoundError",
 ]
---
+++ dataset-catalog-client/tests/models/test_metadata.py
@@ -0,0 +1,52 @@
+from catalog_client.models.governance import GovernanceMetadata
+from catalog_client.models.quality import DataQualityChecks
+from catalog_client.models.metadata import (
+    DatasetMetadata,
+    ExperimentMetadata,
+    OntologyEntry,
+    SampleMetadata,
+    DataSummaryMetadata,
+    TissueEntry,
+)
+
+
+def test_ontology_entry():
+    entry = OntologyEntry(label="Homo sapiens", ontology_id="NCBITaxon:9606")
+    assert entry.label == "Homo sapiens"
+
+
+def test_sample_metadata_defaults_to_none():
+    s = SampleMetadata()
+    assert s.organism is None
+    assert s.tissue is None
+
+
+def test_experiment_metadata_assay_is_list_of_str():
+    e = ExperimentMetadata(assay=["10x Chromium"], assay_ontology_id=["EFO:0009922"])
+    assert e.assay == ["10x Chromium"]
+    assert e.assay_ontology_id == ["EFO:0009922"]
+
+
+def test_dataset_metadata_nests_sub_models():
+    m = DatasetMetadata(
+        sample=SampleMetadata(
+            organism=[OntologyEntry(label="Homo sapiens", ontology_id="NCBITaxon:9606")]
+        ),
+        experiment=ExperimentMetadata(assay=["scRNA-seq"]),
+    )
+    assert m.sample.organism[0].label == "Homo sapiens"
+    assert m.experiment.assay == ["scRNA-seq"]
+    assert m.data_summary is None
+
+
+def test_governance_metadata_extra_fields_allowed():
+    g = GovernanceMetadata(data_owner="team-x", custom_field="value")
+    assert g.data_owner == "team-x"
+    assert g.model_extra["custom_field"] == "value"
+
+
+def test_data_quality_checks():
+    q = DataQualityChecks(checks_passed=["format_check"], checks_failed=[])
+    assert q.checks_passed == ["format_check"]
+    assert q.checks_failed == []
+    assert q.checks_skipped is None
